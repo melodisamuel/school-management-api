18f6c70edfa7099da0c25240427ae573ee788579
Melody <melodysamuel126@gmail.com>
2026-02-17 15:59:20 +0100

    feat: expanded user schema validation to include email, password, and role

--- managers/entities/user/user.schema.js
+++ managers/entities/user/user.schema.js
@@ -1,12 +1,24 @@
-
-
 module.exports = {
     createUser: [
         {
             model: 'username',
             required: true,
         },
+        {
+            model: 'email',
+            required: true,
+        },
+        {
+            model: 'password',
+            required: true,
+        },
+        {
+            model: 'role',
+            required: false, // Defaulted in manager if missing
+        },
+        {
+            model: 'schoolId',
+            required: false,
+        }
     ],
 }
-
-
